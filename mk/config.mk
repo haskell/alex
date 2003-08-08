87a984a8cb1f5980e9f91403583763a622c7a47c
simonmar <unknown>
2003-08-08 13:39:03 +0000

    [alex @ 2003-08-08 13:39:03 by simonmar] Move over from fptools/happy/alex/mk.

---
+++ mk/config.mk
@@ -0,0 +1,12 @@
+#
+# Alex project information
+#
+
+# what to include in a binary distribution
+AlexMainDir 		= alex
+AlexBinDistDirs 	= alex
+AlexBinDistDocs 	= alex/doc
+AlexBinDistShScripts 	= alex-$(ProjectVersion)
+AlexBinDistLinks 	= alex
+
+include $(AlexMainDir)/mk/version.mk
---
+++ mk/version.mk
@@ -0,0 +1,18 @@
+#
+# Project-specific version information.
+#
+# Note:
+#   this config file is intended to centralise all
+#   project version information. To bump up the version
+#   info on your package, edit this file and recompile
+#   all the dependents. This file lives in the source tree.
+#
+
+#
+# happy project variable settings:
+#
+ProjectName       = Alex
+ProjectNameShort  = alex
+ProjectVersion    = 2.0
+ProjectVersionInt = 200
+ProjectPatchLevel = 0
