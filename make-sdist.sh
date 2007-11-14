363ff957e28f44957ddbbbe2092efe323bdbce49
Simon Marlow <simonmar@microsoft.com>
2007-11-14 15:02:57 +0000

    add release instructions

--- make-sdist.sh
+++ make-sdist.sh
@@ -11,3 +11,20 @@ mkdir dist/build
 mv alex dist/build
 cd ..
 tar cvzf alex-*.tar.gz alex-*/
+
+# Steps for doing a release:
+#  * Source:
+#    - do the above
+#    - upload the dist to haskell.org:alex/dist/${version}
+#  * Documentation:
+#    - cd doc
+#    - make html
+#    - mv alex alex-html
+#    - tar cvzf alex-doc-html-${version}.tar.gz alex-html
+#    - scp alex-doc-html-${version}.tar.gz haskell.org:alex/doc
+#    - ssh haskell.org
+#        - cd alex/doc
+#        - tar xvzf alex-doc-html-${version}.tar.gz
+#        - rm -rf html-OLD
+#        - mv html html-OLD && mv alex-html html
+#  * Update the web page (~/darcs/www/alex/index.html), and push it
