ee86d04e8363e74a0b923497d36669ac4dbad8b1
Simon Marlow <marlowsd@gmail.com>
2008-10-13 08:31:06 +0000

    special sdist steps aren't necessary now

--- make-sdist.sh
+++ make-sdist.sh
@@ -1,16 +1,19 @@
 # Put the Happy-generated .hs files in the right place in the source dist.
-set -e
-rm -f dist/alex-*.tar.gz
-rm -rf dist/alex-*/
-./Setup sdist
-cd dist
-tar xvzf alex-*.tar.gz
-cd alex-*/
-mkdir dist
-mkdir dist/build
-mv alex dist/build
-cd ..
-tar cvzf alex-*.tar.gz alex-*/
+
+# Not necessary any more, Cabal does this:
+
+# set -e
+# rm -f dist/alex-*.tar.gz
+# rm -rf dist/alex-*/
+# ./Setup sdist
+# cd dist
+# tar xvzf alex-*.tar.gz
+# cd alex-*/
+# mkdir dist
+# mkdir dist/build
+# mv alex dist/build
+# cd ..
+# tar cvzf alex-*.tar.gz alex-*/
 
 # Steps for doing a release:
 #  * Source:
