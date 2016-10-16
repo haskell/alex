haskell/alex

commit 00431304aa18ada0a12aab43443023a86521f353

Got basic template working with type classes

src/Output.hs
@@ -21,17 +21,19 @@ import Data.Array ( Array )
 import Data.Array.Base ( unsafeRead )
 import Data.Array.ST ( STUArray, newArray, readArray, writeArray, freeze )
 import Data.Array.Unboxed ( UArray, elems, (!), array, listArray )
+import Data.Maybe (isJust)
 import Data.Bits
 import Data.Char ( ord, chr )
-import Data.List ( maximumBy, sortBy, groupBy )
+import Data.List ( maximumBy, sortBy, groupBy, mapAccumR )
 
 -- -----------------------------------------------------------------------------
 -- Printing the output
 
-outputDFA :: Target -> Int -> String -> Maybe (Maybe String, String) -> DFA SNum Code -> ShowS
-outputDFA target _ _ typesig dfa
+outputDFA :: Target -> Int -> String -> Scheme -> DFA SNum Code -> ShowS
+outputDFA target _ _ scheme dfa
   = interleave_shows nl
-        [outputBase, outputTable, outputCheck, outputDefault, outputAccept]
+        [outputBase, outputTable, outputCheck, outputDefault,
+         outputAccept, outputActions, outputSigs]
   where
     (base, table, check, deflt, accept) = mkTables dfa
 
@@ -43,6 +45,7 @@ outputDFA target _ _ typesig dfa
     check_nm  = "alex_check"
     deflt_nm  = "alex_deflt"
     accept_nm = "alex_accept"
+    actions_nm = "alex_actions"
 
     outputBase    = do_array hexChars32 base_nm  n_states   base
     outputTable   = do_array hexChars16 table_nm table_size table
@@ -63,43 +66,89 @@ outputDFA target _ _ typesig dfa
         . str ") [" . interleave_shows (char ',') (map shows ints)
         . str "]\n"
 
-    outputAccept
-        = case typesig of
-          Nothing ->
+    outputAccept =
+        str accept_nm . str " :: Array Int (AlexAcc ())\n"
+      . str accept_nm . str " = listArray (0::Int," . shows n_states . str ") ["
+      . interleave_shows (char ',') (snd (mapAccumR outputAccs 0 accept))
+      . str "]\n"
+
+    outputActions
+        = let
+            (nacts, acts) = mapAccumR outputActs 0 accept
+          in case scheme of
+          Basic { basicByteString = isByteString,
+                  basicTypeInfo = Just (Nothing, toktype) } ->
+              str actions_nm . str " :: Array Int ("
+            . str (strtype isByteString) . str " -> " . str toktype
+            . str ")\n"
+            . str actions_nm . str " = array (0::Int," . shows nacts
+            . str ") [" . interleave_shows (char ',') (concat acts)
+            . str "]\n"
+          Basic { basicByteString = isByteString,
+                  basicTypeInfo = Just (Just tyclasses, toktype) } ->
+              str actions_nm . str " :: (" . str tyclasses
+            . str ") => Array Int ("
+            . str (strtype isByteString) . str " -> " . str toktype
+            . str ")\n"
+            . str actions_nm . str " = array (0::Int," . shows nacts
+            . str ") [" . interleave_shows (char ',') (concat acts)
+            . str "]\n"
+          _ ->
               -- No type signature: we don't know what the type of the actions is.
               -- str accept_nm . str " :: Array Int (Accept Code)\n"
-              str accept_nm . str " = listArray (0::Int," . shows n_states
-            . str ") [" . interleave_shows (char ',') (map outputAccs accept)
-            . str "]\n"
-          Just (Nothing, rettype) ->
-              str accept_nm . str " :: Array Int (AlexAcc ("
-            . str rettype . str "))\n"
-            . str accept_nm . str " = listArray (0::Int," . shows n_states
-            . str ") [" . interleave_shows (char ',') (map outputAccs accept)
-            . str "]\n"
-          Just (Just tyclasses, rettype) ->
-              str accept_nm . str " :: (" . str tyclasses
-            . str ") => Array Int (AlexAcc (" . str rettype . str "))\n"
-            . str accept_nm . str " = listArray (0::Int," . shows n_states
-            . str ") [" . interleave_shows (char ',') (map outputAccs accept)
+              str actions_nm . str " = array (0::Int," . shows nacts
+            . str ") [" . interleave_shows (char ',') (concat acts)
             . str "]\n"
 
-    outputAccs :: [Accept Code] -> ShowS
-    outputAccs []
-        = str "AlexAccNone"
-    outputAccs (Acc _ Nothing Nothing NoRightContext : [])
-        = str "AlexAccSkip"
-    outputAccs (Acc _ (Just act) Nothing NoRightContext : [])
-        = str "AlexAcc " . paren (str act)
-    outputAccs (Acc _ Nothing lctx rctx : rest)
-        = str "AlexAccSkipPred " . space
-        . paren (outputPred lctx rctx)
-        . paren (outputAccs rest)
-    outputAccs (Acc _ (Just act) lctx rctx : rest)
-        = str "AlexAccPred " . space
-        . paren (str act) . space
-        . paren (outputPred lctx rctx)
-        . paren (outputAccs rest)
+    outputSigs
+        = case scheme of
+          Basic { basicByteString = isByteString,
+                  basicTypeInfo = Just (Nothing, toktype) } ->
+              str "alex_scan_tkn :: () -> AlexInput -> Int -> "
+            . str "AlexInput -> Int -> AlexLastAcc -> (AlexLastAcc, AlexInput)\n"
+            . str "alexScanUser :: () -> AlexInput -> Int -> AlexReturn ("
+            . str (strtype isByteString) . str " -> " . str toktype . str ")\n"
+            . str "alexScan :: AlexInput -> Int -> AlexReturn ("
+            . str (strtype isByteString) . str " -> " . str toktype . str ")\n"
+          Basic { basicByteString = isByteString,
+                  basicTypeInfo = Just (Just tyclasses, toktype) } ->
+              str "alex_scan_tkn :: () -> AlexInput -> Int -> "
+            . str "AlexInput -> Int -> AlexLastAcc -> (AlexLastAcc, AlexInput)\n"
+            . str "alexScanUser :: (" . str tyclasses
+            . str ") => () -> AlexInput -> Int -> AlexReturn ("
+            . str (strtype isByteString) . str " -> " . str toktype . str ")\n"
+            . str "alexScan :: (" . str tyclasses
+            . str ") => AlexInput -> Int -> AlexReturn ("
+            . str (strtype isByteString) . str " -> " . str toktype . str ")\n"
+          _ ->
+              str ""
+
+    outputAccs :: Int -> [Accept Code] -> (Int, ShowS)
+    outputAccs idx [] = (idx, str "AlexAccNone")
+    outputAccs idx (Acc _ Nothing Nothing NoRightContext : [])
+      = (idx, str "AlexAccSkip")
+    outputAccs idx (Acc _ (Just _) Nothing NoRightContext : [])
+      = (idx + 1, str "AlexAcc " . str (show idx))
+    outputAccs idx (Acc _ Nothing lctx rctx : rest)
+      = let (idx', rest') = outputAccs idx rest
+        in (idx', str "AlexAccSkipPred" . space
+                 . paren (outputPred lctx rctx)
+                 . paren rest')
+    outputAccs idx (Acc _ (Just _) lctx rctx : rest)
+      = let (idx', rest') = outputAccs (idx + 1) rest
+        in (idx', str "AlexAccPred" . space
+                  . str (show idx) . space
+                  . paren (outputPred lctx rctx)
+                  . paren rest')
+
+    outputActs :: Int -> [Accept Code] -> (Int, [ShowS])
+    outputActs idx =
+      let
+        outputAct _ (Acc _ Nothing _ _) = error "Shouldn't see this"
+        outputAct idx (Acc _ (Just act) _ _) =
+          (idx + 1, paren (shows idx . str "," . str act))
+      in
+        mapAccumR outputAct idx . filter (\(Acc _ act _ _) -> isJust act)
 
     outputPred (Just set) NoRightContext
         = outputLCtx set

src/AbsSyn.hs
@@ -10,12 +10,13 @@
 -- ----------------------------------------------------------------------------}
 
 module AbsSyn (
-  Code, Directive(..),
+  Code, Directive(..), Scheme(..),
+  wrapperName,
   Scanner(..),
   RECtx(..),
   RExp(..),
   DFA(..), State(..), SNum, StartCode, Accept(..),
-  RightContext(..), showRCtx,
+  RightContext(..), showRCtx, strtype,
   encodeStartCodes, extractActions,
   Target(..),
   UsesPreds(..), usesPreds
@@ -43,8 +44,27 @@ data Directive
    | EncodingDirective Encoding         -- use this encoding
    | ActionType (Maybe String) String   -- Type signature of actions,
                                         -- with optional typeclasses
+   | TypeClass String
    | TokenType String
    deriving Show
+
+data Scheme
+  = Default
+  | Basic { basicByteString :: Bool,
+            basicTypeInfo :: Maybe (Maybe String, String) }
+  | Posn { posnByteString :: Bool,
+           posnTypeInfo :: Maybe (Maybe String, String) }
+
+strtype :: Bool -> String
+strtype True = "Data.ByteString.Lazy.ByteString"
+strtype False = "String"
+
+wrapperName :: Scheme -> Maybe String
+wrapperName Default {} = Nothing
+wrapperName Basic { basicByteString = False } = Just "basic"
+wrapperName Basic { basicByteString = True } = Just "basic-bytestring"
+wrapperName Posn { posnByteString = False } = Just "posn"
+wrapperName Posn { posnByteString = True } = Just "posn-bytestring"
 
 -- TODO: update this comment
 --
@@ -278,8 +298,8 @@ encodeStartCodes scan = (scan', 0 : map snd name_code_pairs, sc_hdr)
 -- because the actual action fragments might be duplicated in the
 -- generated file.
 
-extractActions :: Maybe (Maybe String, String) -> Scanner -> (Scanner,ShowS)
-extractActions actionty scanner = (scanner{scannerTokens = new_tokens}, decl_str)
+extractActions :: Scheme -> Scanner -> (Scanner,ShowS)
+extractActions scheme scanner = (scanner{scannerTokens = new_tokens}, decl_str)
  where
   (new_tokens, decls) = unzip (zipWith f (scannerTokens scanner) act_names)
 
@@ -288,15 +308,18 @@ extractActions actionty scanner = (scanner{scannerTokens = new_tokens}, decl_str
   f r@RECtx{ reCtxCode = Nothing } _
         = (r{reCtxCode = Nothing}, Nothing)
 
-  mkDecl fun code = case actionty of
-    Nothing -> str fun . str " = " . str code . nl
-    Just (Nothing, actionty') ->
-      str fun . str " :: " . str actionty' . str "\n" .
-      str fun . str " = " . str code . nl
-    Just (Just tyclasses, actionty') ->
+  mkDecl fun code = case scheme of
+    Basic { basicByteString = isByteString,
+            basicTypeInfo = Just (Nothing, tokenty) } ->
+      str fun . str " :: " . str (strtype isByteString) . str " -> "
+      . str tokenty . str "\n"
+      . str fun . str " = " . str code . nl
+    Basic { basicByteString = isByteString,
+            basicTypeInfo = Just (Just tyclasses, tokenty) } ->
       str fun . str " :: (" . str tyclasses . str ") => " .
-      str actionty' . str "\n" .
+      str (strtype isByteString) . str " -> " . str tokenty . str "\n" .
       str fun . str " = " . str code . nl
+    _ -> str fun . str " = " . str code . nl
 
   act_names = map (\n -> "alex_action_" ++ show (n::Int)) [0..]
 

src/Main.hs
@@ -150,7 +150,7 @@ alex cli file basename script = do
 
    let (maybe_header, directives, scanner1, maybe_footer) = script
 
-   actty_info <- actionType directives
+   scheme <- getScheme directives
 
    -- open the output file; remove it if we encounter an error
    bracketOnError
@@ -159,9 +159,9 @@ alex cli file basename script = do
         $ \out_h -> do
 
    let
+         wrapper_name = wrapperFile template_dir scheme
          (scanner2, scs, sc_hdr) = encodeStartCodes scanner1
-         (scanner_final, actions) = extractActions actty_info scanner2
-
+         (scanner_final, actions) = extractActions scheme scanner2
          encodingsScript = [ e | EncodingDirective e <- directives ]
 
    encoding <- case nub (encodingsCli ++ encodingsScript) of
@@ -169,8 +169,6 @@ alex cli file basename script = do
      [e] -> return e
      _ | null encodingsCli -> dieAlex "conflicting %encoding directives"
        | otherwise -> dieAlex "--latin1 flag conflicts with %encoding directive"
-
-   wrapper_name <- wrapperFile template_dir directives
 
    hPutStr out_h (optsToInject target cli)
    injectCode maybe_header file out_h
@@ -202,7 +200,7 @@ alex cli file basename script = do
    put_info (infoDFA 1 nm dfa "")
    put_info "\nMinimized DFA"
    put_info (infoDFA 1 nm min_dfa "")
-   hPutStr out_h (outputDFA target 1 nm actty_info min_dfa "")
+   hPutStr out_h (outputDFA target 1 nm scheme min_dfa "")
 
    injectCode maybe_footer file out_h
 
@@ -216,6 +214,62 @@ alex cli file basename script = do
 
    hClose out_h
    finish_info
+
+actionType :: [Directive] -> IO (Maybe (Maybe String, String))
+actionType directives =
+  case [ (tyclasses, actty) | ActionType tyclasses actty <- directives ] of
+    [] -> return Nothing
+    [res] -> return (Just res)
+    _ -> dieAlex "multiple %actiontype directives"
+
+getScheme :: [Directive] -> IO Scheme
+getScheme directives =
+  do
+    token <- case [ ty | TokenType ty <- directives ] of
+      [] -> return Nothing
+      [res] -> return (Just res)
+      _ -> dieAlex "multiple %token directives"
+
+    typeclass <- case [ tyclass | TypeClass tyclass <- directives ] of
+      [] -> return Nothing
+      [res] -> return (Just res)
+      _ -> dieAlex "multiple %typeclass directives"
+
+    case [ f | WrapperDirective f <- directives ] of
+        []  -> return Default
+        [single]
+          | single == "basic" || single == "basic-bytestring" ->
+            let
+              isByteString = single == "basic-bytestring"
+            in case (typeclass, token) of
+              (Nothing, Nothing) ->
+                return Basic { basicByteString = isByteString,
+                               basicTypeInfo = Nothing }
+              (Nothing, Just tokenty) ->
+                return Basic { basicByteString = isByteString,
+                               basicTypeInfo = Just (Nothing, tokenty) }
+              (Just _, Just tokenty) ->
+                return Basic { basicByteString = isByteString,
+                               basicTypeInfo = Just (typeclass, tokenty) }
+              (Just _, Nothing) ->
+                dieAlex "%typeclass directive without %token directive"
+          | single == "posn" || single == "posn-bytestring" ->
+            let
+              isByteString = single == "posn-bytestring"
+            in case (typeclass, token) of
+              (Nothing, Nothing) ->
+                return Posn { posnByteString = isByteString,
+                              posnTypeInfo = Nothing }
+              (Nothing, Just tokenty) ->
+                return Posn { posnByteString = isByteString,
+                              posnTypeInfo = Just (Nothing, tokenty) }
+              (Just _, Just tokenty) ->
+                return Posn { posnByteString = isByteString,
+                              posnTypeInfo = Just (typeclass, tokenty) }
+              (Just _, Nothing) ->
+                dieAlex "%typeclass directive without %token directive"
+          | otherwise -> dieAlex ("unknown wrapper type " ++ single)
+        _many -> dieAlex "multiple %wrapper directives"
 
 -- inject some code, and add a {-# LINE #-} pragma at the top
 injectCode :: Maybe (AlexPosn,Code) -> FilePath -> Handle -> IO ()
@@ -298,19 +352,11 @@ templateFile dir target usespreds cli
                           && null maybe_debug -> "-nopred"
             _                                 -> ""
 
-wrapperFile :: FilePath -> [Directive] -> IO (Maybe FilePath)
-wrapperFile dir directives =
-  case [ f | WrapperDirective f <- directives ] of
-        []  -> return Nothing
-        [f] -> return (Just (dir ++ "/AlexWrapper-" ++ f))
-        _many -> dieAlex "multiple %wrapper directives"
-
-actionType :: [Directive] -> IO (Maybe (Maybe String, String))
-actionType directives =
-  case [ (tyclasses, actty) | ActionType tyclasses actty <- directives ] of
-    [] -> return Nothing
-    [res] -> return (Just res)
-    _ -> dieAlex "multiple %actiontype directives"
+wrapperFile :: FilePath -> Scheme -> Maybe FilePath
+wrapperFile dir scheme =
+  do
+    f <- wrapperName scheme
+    return (dir ++ "/AlexWrapper-" ++ f)
 
 infoStart :: FilePath -> FilePath -> IO (String -> IO (), IO ())
 infoStart x_file info_file = do

src/Parser.y
@@ -62,6 +62,7 @@ import Data.Char
 	ENCODING	{ T _ EncodingT }
         ACTIONTYPE      { T _ ActionTypeT }
         TOKENTYPE       { T _ TokenTypeT }
+        TYPECLASS       { T _ TypeClassT }
 %%
 
 alex	:: { (Maybe (AlexPosn,Code), [Directive], Scanner, Maybe (AlexPosn,Code)) }
@@ -82,6 +83,7 @@ directive  :: { Directive }
         | ACTIONTYPE STRING             { ActionType Nothing $2 }
         | ACTIONTYPE STRING STRING      { ActionType (Just $2) $3 }
         | TOKENTYPE STRING              { TokenType $2 }
+        | TYPECLASS STRING              { TypeClass $2 }
 
 encoding :: { Encoding }
         : STRING         		{% lookupEncoding $1 }

src/Scan.x
@@ -52,6 +52,7 @@ alex :-
 <0> \% "encoding"		{ encoding }
 <0> \% "action"                 { actionty }
 <0> \% "token"                  { tokenty }
+<0> \% "typeclass"              { typeclass }
 
 <0> \\ $digit+			{ decch }
 <0> \\ x $hexdig+		{ hexch }
@@ -102,6 +103,7 @@ data Tkn
  | EncodingT
  | ActionTypeT
  | TokenTypeT
+ | TypeClassT
  | EOFT
  deriving Show
 
@@ -126,6 +128,7 @@ wrapper   (p,_,str) ln = return $ T p WrapperT
 encoding  (p,_,str) ln = return $ T p EncodingT
 actionty  (p,_,str) ln = return $ T p ActionTypeT
 tokenty   (p,_,str) ln = return $ T p TokenTypeT
+typeclass (p,_,str) ln = return $ T p TypeClassT
 
 isIdChar c = isAlphaNum c || c `elem` "_'"
 

templates/GenericTemplate.hs
@@ -74,7 +74,7 @@ alexIndexInt16OffAddr arr off = arr ! off
 
 #ifdef ALEX_GHC
 {-# INLINE alexIndexInt32OffAddr #-}
-alexIndexInt32OffAddr (AlexA# arr) off = 
+alexIndexInt32OffAddr (AlexA# arr) off =
 ALEX_IF_BIGENDIAN
   narrow32Int# i
   where
@@ -120,9 +120,9 @@ alexScan input IBOX(sc)
 
 alexScanUser user input IBOX(sc)
   = case alex_scan_tkn user input ILIT(0) input sc AlexNone of
-        (AlexNone, input') ->
-                case alexGetByte input of
-                        Nothing -> 
+	(AlexNone, input') ->
+		case alexGetByte input of
+			Nothing ->
 #ifdef ALEX_DEBUG
                                    trace ("End of input.") $
 #endif
@@ -135,15 +135,15 @@ alexScanUser user input IBOX(sc)
 
         (AlexLastSkip input'' len, _) ->
 #ifdef ALEX_DEBUG
-                trace ("Skipping.") $ 
+		trace ("Skipping.") $
 #endif
                 AlexSkip input'' len
 
         (AlexLastAcc k input''' len, _) ->
 #ifdef ALEX_DEBUG
-                trace ("Accept.") $ 
+		trace ("Accept.") $
 #endif
-                AlexToken input''' len k
+		AlexToken input''' len (alex_actions ! k)
 
 
 -- Push the input through the DFA, remembering the most recent accepting
@@ -151,13 +151,13 @@ alexScanUser user input IBOX(sc)
 
 alex_scan_tkn user orig_input len input s last_acc =
   input `seq` -- strict in the input
-  let 
-        new_acc = (check_accs (alex_accept `quickIndex` IBOX(s)))
+  let
+	new_acc = (check_accs (alex_accept `quickIndex` IBOX(s)))
   in
   new_acc `seq`
   case alexGetByte input of
      Nothing -> (new_acc, input)
-     Just (c, new_input) -> 
+     Just (c, new_input) ->
 #ifdef ALEX_DEBUG
       trace ("State: " ++ show IBOX(s) ++ ", char: " ++ show c) $
 #endif
@@ -166,7 +166,7 @@ alex_scan_tkn user orig_input len input s last_acc =
                 base   = alexIndexInt32OffAddr alex_base s
                 offset = PLUS(base,ord_c)
                 check  = alexIndexInt16OffAddr alex_check offset
-                
+
                 new_s = if GTE(offset,ILIT(0)) && EQ(check,ord_c)
                           then alexIndexInt16OffAddr alex_table offset
                           else alexIndexInt16OffAddr alex_deflt s
@@ -196,23 +196,18 @@ alex_scan_tkn user orig_input len input s last_acc =
            = check_accs rest
 #endif
 
-data AlexLastAcc a
+data AlexLastAcc
   = AlexNone
-  | AlexLastAcc a !AlexInput !Int
-  | AlexLastSkip  !AlexInput !Int
+  | AlexLastAcc !Int !AlexInput !Int
+  | AlexLastSkip     !AlexInput !Int
 
-instance Functor AlexLastAcc where
-    fmap _ AlexNone = AlexNone
-    fmap f (AlexLastAcc x y z) = AlexLastAcc (f x) y z
-    fmap _ (AlexLastSkip x y) = AlexLastSkip x y
-
-data AlexAcc a user
+data AlexAcc user
   = AlexAccNone
-  | AlexAcc a
+  | AlexAcc Int
   | AlexAccSkip
 #ifndef ALEX_NOPRED
-  | AlexAccPred a   (AlexAccPred user) (AlexAcc a user)
-  | AlexAccSkipPred (AlexAccPred user) (AlexAcc a user)
+  | AlexAccPred Int (AlexAccPred user) (AlexAcc user)
+  | AlexAccSkipPred (AlexAccPred user) (AlexAcc user)
 
 type AlexAccPred user = user -> AlexInput -> Int -> AlexInput -> Bool
 
@@ -222,16 +217,16 @@ type AlexAccPred user = user -> AlexInput -> Int -> AlexInput -> Bool
 alexAndPred p1 p2 user in1 len in2
   = p1 user in1 len in2 && p2 user in1 len in2
 
---alexPrevCharIsPred :: Char -> AlexAccPred _ 
+--alexPrevCharIsPred :: Char -> AlexAccPred _
 alexPrevCharIs c _ input _ _ = c == alexInputPrevChar input
 
 alexPrevCharMatches f _ input _ _ = f (alexInputPrevChar input)
 
---alexPrevCharIsOneOfPred :: Array Char Bool -> AlexAccPred _ 
+--alexPrevCharIsOneOfPred :: Array Char Bool -> AlexAccPred _
 alexPrevCharIsOneOf arr _ input _ _ = arr ! alexInputPrevChar input
 
 --alexRightContext :: Int -> AlexAccPred _
-alexRightContext IBOX(sc) user _ _ input = 
+alexRightContext IBOX(sc) user _ _ input =
      case alex_scan_tkn user input ILIT(0) input sc AlexNone of
           (AlexNone, _) -> False
           _ -> True

commit 68eefd904c5f69045c5609f115942c5741709081

alex: fix generated type for 'alex_accept'
The change fixes lexer generation failure for GHC lexer.
GHC lexer is special because it calls `alexScanUser`
to thread lexer state.

Added minimal test `tokens_scan_user.x`
to cover `alexScanUser` use.

Signature was reintroduced in commit 00431304aa18ada0a12aab43443023a86521f353
accidentally.

Fixes https://github.com/simonmar/alex/issues/98

Signed-off-by: Sergei Trofimovich <siarheit@google.com>

alex.cabal
@@ -61,6 +61,7 @@ extra-source-files:
         tests/tokens_posn.x
         tests/tokens_bytestring.x
         tests/tokens_posn_bytestring.x
+        tests/tokens_scan_user.x
         tests/tokens_strict_bytestring.x
         tests/tokens_monad_bytestring.x
         tests/tokens_monadUserState_bytestring.x

src/Output.hs
@@ -71,14 +71,10 @@ outputDFA target _ _ scheme dfa
         . str "]\n"
 
     outputAccept =
-      let
-        userStateTy = case scheme of
-          Monad { monadUserState = True } -> "AlexUserState"
-          _ -> "()"
-      in
-          str accept_nm . str " :: Array Int (AlexAcc " . str userStateTy
-        . str ")\n" . str accept_nm
-        . str " = listArray (0::Int," . shows n_states . str ") ["
+        -- Don't emit explicit type signature as it contains unknown user type,
+        -- see: https://github.com/simonmar/alex/issues/98
+        -- str accept_nm . str " :: Array Int (AlexAcc " . str userStateTy . str ")\n"
+        str accept_nm . str " = listArray (0::Int," . shows n_states . str ") ["
         . interleave_shows (char ',') (snd (mapAccumR outputAccs 0 accept))
         . str "]\n"
 

tests/Makefile
@@ -10,7 +10,31 @@ else
 HS_PROG_EXT = .bin
 endif
 
-TESTS = unicode.x simple.x tokens.x tokens_posn.x tokens_gscan.x tokens_bytestring.x tokens_posn_bytestring.x tokens_strict_bytestring.x tokens_monad_bytestring.x tokens_monadUserState_bytestring.x null.x tokens_bytestring_unicode.x basic_typeclass.x basic_typeclass_bytestring.x strict_typeclass.x posn_typeclass.x posn_typeclass_bytestring.x gscan_typeclass.x monad_typeclass.x monad_typeclass_bytestring.x monadUserState_typeclass.x monadUserState_typeclass_bytestring.x default_typeclass.x
+TESTS = \
+        basic_typeclass.x \
+        basic_typeclass_bytestring.x \
+        default_typeclass.x \
+        gscan_typeclass.x \
+        monad_typeclass.x \
+        monad_typeclass_bytestring.x \
+        monadUserState_typeclass.x \
+        monadUserState_typeclass_bytestring.x \
+        null.x \
+        posn_typeclass.x \
+        posn_typeclass_bytestring.x \
+        strict_typeclass.x \
+        simple.x \
+        tokens.x \
+        tokens_bytestring.x \
+        tokens_bytestring_unicode.x \
+        tokens_gscan.x \
+        tokens_monad_bytestring.x \
+        tokens_monadUserState_bytestring.x \
+        tokens_posn.x \
+        tokens_posn_bytestring.x \
+        tokens_scan_user.x \
+        tokens_strict_bytestring.x \
+        unicode.x
 
 TEST_ALEX_OPTS = --template=..
 

tests/tokens_scan_user.x
@@ -1,0 +1,50 @@
+{
+module Main (main) where
+import System.Exit
+}
+
+%wrapper "basic" -- Defines: AlexInput, alexGetByte, alexPrevChar
+
+$digit = 0-9
+$alpha = [a-zA-Z]
+$ws    = [\ \t\n]
+
+tokens :-
+
+  5 / {\u _ibt _l _iat -> u == FiveIsMagic} { \s -> TFive (head s) }
+  $digit { \s -> TDigit (head s) }
+  $alpha { \s -> TAlpha (head s) }
+  $ws    { \s -> TWSpace (head s) }
+
+{
+
+data Token = TDigit Char
+           | TAlpha Char
+           | TWSpace Char
+           | TFive Char -- Predicated only
+           | TLexError
+    deriving (Eq,Show)
+
+data UserLexerMode = NormalMode
+                   | FiveIsMagic
+    deriving Eq
+
+main | test1 /= result1 = exitFailure
+     | test2 /= result2 = exitFailure
+     -- all succeeded
+     | otherwise        = exitWith ExitSuccess
+
+run_lexer :: UserLexerMode -> String -> [Token]
+run_lexer m s = go ('\n', [], s)
+    where go i@(_,_,s') = case alexScanUser m i 0 of
+                     AlexEOF             -> []
+                     AlexError  _i       -> [TLexError]
+                     AlexSkip   i' _len  ->                   go i'
+                     AlexToken  i' len t -> t (take len s') : go i'
+
+test1 = run_lexer FiveIsMagic "5 x"
+result1 = [TFive '5',TWSpace ' ',TAlpha 'x']
+
+test2 = run_lexer NormalMode "5 x"
+result2 = [TDigit '5',TWSpace ' ',TAlpha 'x']
+}