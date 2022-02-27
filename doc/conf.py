90b138642739e9735a091a64df09bd52512cd638
Andreas Abel <andreas.abel@ifi.lmu.de>
2022-02-27 18:44:09 +0100

    RST docs: choose theme (sphinx_rtd) and highlighting style (sphinx)
    There is no `alex` pygments highlighter, unfortunately.  The one for
    `peg` looks reasonable for grammar definitions.

--- doc/conf.py
+++ doc/conf.py
@@ -44,9 +44,15 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+highlight_language = 'Peg'
+# Andreas Abel, 2022-02-27: Peg looked best from the "grammar" highlighters.
+# I also tried 'Bnf' and 'Abnf'.
