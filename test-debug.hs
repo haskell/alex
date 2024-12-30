e0b73c09ad7b42371da9aa00b2e44162dbca8fdf
Andreas Abel <andreas.abel@ifi.lmu.de>
2024-12-30 09:30:11 +0100

    Separate debug tests into own testsuite tests-debug

---
+++ test-debug.hs
@@ -0,0 +1,4 @@
+import System.Process (system)
+import System.Exit (exitWith)
+
+main = system "make -k -C tests clean tests-debug" >>= exitWith
--- test.hs
+++ test.hs
@@ -1,4 +1,4 @@
 import System.Process (system)
 import System.Exit (exitWith)
 
-main = system "make -k -C tests clean all" >>= exitWith
+main = system "make -k -C tests clean tests" >>= exitWith
